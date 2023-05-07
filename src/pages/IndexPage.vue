27015d3fbb9d648b886be716e81a24174b7baeb4
Nikita Solovev <solovev.nikita@mail.ru>
2023-05-07 13:50:05 +0500

    add quasar

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <SimpleBikeTrack />
+  </q-page>
+</template>
+
+<script lang="ts">
+import SimpleBikeTrack from "components/SimpleBikeTrack.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {};
+  },
+  components: {
+    SimpleBikeTrack,
+  },
+});
+</script>
